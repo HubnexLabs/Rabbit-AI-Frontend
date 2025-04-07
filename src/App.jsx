5b68498cb5d34359bc7bf638091fe550af4a9a31
Shubham Liman <shubhamlstudymail@gmail.com>
2025-04-07 12:09:10 +0530

    Initialize rabbitAI frontend

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="text-black h-screen w-screen flex justify-center items-center">
+      Hello world
+    </div>
+  );
+}
+
+export default App;
