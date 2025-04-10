0ce18ef1dd998f0c00227234345e8f5360e8e9d5
Shubham Liman <shubhamlstudymail@gmail.com>
2025-04-10 11:08:25 +0530

    -

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function App() {
   return (
-    <div className="text-black h-screen w-screen flex justify-center items-center">
-      Hello world
+    <div className="text-amber-600 h-screen w-screen flex justify-center items-center ">
+      <p className="border-2 border-black rounded-[5px]"> Hello world</p>
     </div>
   );
 }
